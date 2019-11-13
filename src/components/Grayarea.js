a4480b8840e26eca2b9f5b4f7c3e0882904c397f
kct285 <kimberlytanchay@Kimberlys-MacBook-Pro.local>
2019-11-13 12:36:25 +0800

    hover only

---
+++ src/components/Grayarea.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import Button from './components/What';
+
+function GrayArea(){
+
+
+    return(
+        <div>
+            <p>You selected: {props.name}</p>
+        </div>
+
+    )
+
+    
+
+    
+
+
+}
+
+
+
+
+export default GrayArea
---
+++ src/components/What.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+function Button (props) {
+    return(
+        <button className ="click"> {props.name} </button>
+
+    )
+
+    
+}
+export default Button
